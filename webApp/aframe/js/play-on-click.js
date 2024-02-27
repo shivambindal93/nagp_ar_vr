b1ce2e98aa2d71d59d9fe6e47833f2fd2d9f059e
shivambindal93 <77718440+shivambindal93@users.noreply.github.com>
2024-02-27 22:43:12 +0530

    update code

--- webApp/aframe/js/play-on-click.js
+++ webApp/aframe/js/play-on-click.js
@@ -1,6 +1,5 @@
 /* global AFRAME */
 AFRAME.registerComponent("play-on-click", {
-  isPaused: true,
   init: function () {
     this.onClick = this.onClick.bind(this);
   },
@@ -11,25 +10,11 @@ AFRAME.registerComponent("play-on-click", {
     window.removeEventListener("click", this.onClick);
   },
   onClick: function (evt) {
-    debugger;
-    var objId = this.el.getAttribute("id");
-    if (!objId) {
+    var videoEl = this.el.getAttribute("material").src;
+    if (!videoEl) {
       return;
     }
-
-    if (objId === "bunny") {
-      const videoEl = this.el.getAttribute("material").src;
-      if (!videoEl) {
-        return;
-      }
-      videoEl.play();
-    } else if (objId === "animatedBox") {
-      this.el.object3D.animation__position =
-        "property: position; to: 0 1 -1; dur: 2000; easing: linear; loop: true";
-      this.el.object3D.animation__position =
-        "property: position; to: 0 1 -1; dur: 2000; easing: linear; loop: true";
-      this.el.animation__position =
-        "property: position; to: 0 1 -1; dur: 2000; easing: linear; loop: true";
-    }
-  },
+    this.el.object3D.visible = true;
+    videoEl.play();
+  }
 });
